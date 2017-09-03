83c943f28f6f32c5c912e0ea5e6a6cb79be6a62e
paikwiki <paikwiki@gmail.com>
2017-09-03 20:42:18 +0900

    Remove custom getDate method

--- modules/useModuleExports.js
+++ modules/useModuleExports.js
@@ -5,9 +5,6 @@ module.exports = function(msg) {
     return {
         getMsg: function() {
             return msg;
-        },
-        getDate: function() {
-            return new Date();            
         }
     };
 }
